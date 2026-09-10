8adfcee70ccd894c43942c0f7b23b2f4ed63a872
SkyareCom <skyare.company@gmail.com>
2026-09-10 14:04:37 -0300

    fix: correct GitHub Pages asset prefix

--- next.config.mjs
+++ next.config.mjs
@@ -16,7 +16,7 @@ const nextConfig = {
         ...(isGitHubPages
           ? {
               basePath: `/${repo}`,
-              assetPrefix: `/${repo}/`,
+              assetPrefix: `/${repo}`,
             }
           : {}),
       }
