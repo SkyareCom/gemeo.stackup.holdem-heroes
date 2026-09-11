e740a3736541d504007a71e971258e50a67ddcc8
SkyareCom <skyare.company@gmail.com>
2026-09-11 01:12:07 -0300

    style: load Player DNA contrast tuning last

--- app/player-dna/page.tsx
+++ app/player-dna/page.tsx
@@ -7,6 +7,7 @@ import "./player-dna-typography.css";
 import "./player-dna-selection-contrast.css";
 import "./player-dna-footer-controls.css";
 import "./player-dna-blue-template.css";
+import "./player-dna-contrast-blue.css";
 
 export default function PlayerDnaPage(){
   return <main className="module-page player-dna-page">
