b8c0239774495794c640be3df3baa404d8f60bef
SkyareCom <skyare.company@gmail.com>
2026-09-14 16:54:37 -0300

    fix: reuse home artwork for internal header

--- components/StackupInternalHeader.tsx
+++ components/StackupInternalHeader.tsx
@@ -1,19 +1,5 @@
-import Link from "next/link";
-import "@/app/internal-visual-system.css";
-
-const HEROES_LOGO_SRC = "/gemeo.stackup.holdem-heroes/stackup-heroes-logo-128-valid-20260911.png";
+import StackupAppHeader from "@/components/StackupAppHeader";
 
 export default function StackupInternalHeader(){
-  return (
-    <header className="stackup-app-header">
-      <Link className="stackup-app-header-link" href="/">
-        <img className="stackup-app-header-logo" src={HEROES_LOGO_SRC} alt="" width="128" height="128" />
-        <span className="stackup-app-header-copy">
-          <span className="stackup-app-header-brand">STACKUP HOLD&apos;EM</span>
-          <span className="stackup-app-header-heroes">HEROES</span>
-          <span className="stackup-app-header-subtitle">AI POKER PERFORMANCE SYSTEM</span>
-        </span>
-      </Link>
-    </header>
-  );
+  return <div className="internal-header-host"><StackupAppHeader/></div>;
 }
