63bdd74a94b255fd216dd7411f417114b84c76ac
SkyareCom <skyare.company@gmail.com>
2026-09-14 11:37:10 -0300

    chore: refresh secondary home-clone header

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ const loveYaLikeASister = Love_Ya_Like_A_Sister({
   variable:"--font-love-ya-like-a-sister",
 });
 
-const CACHE_RESET_VERSION = "20260914-v16-secondary-header";
+const CACHE_RESET_VERSION = "20260914-v17-secondary-home-clone";
 
 export const metadata: Metadata = {
   title: "STACKUP HOLD'EM HEROES",
